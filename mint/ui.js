2ed0720b3938d0f07f75dd109cb7f6c71d10e95f
Jessica Dally <jessica.dally@me.com>
2021-12-21 15:14:46 -0700

    Update ui.js

--- mint/ui.js
+++ mint/ui.js
@@ -2,54 +2,6 @@ import { mint } from "./web3.js";
 import { showAlert } from "../ui/alerts.js";
 import { parseTxError } from "../utils.js";
 
-export const updateMintHolderButton = () => {
-    const mintholderButton = document.querySelector('#holder-mint-button');
-    if (mintholderButton) {
-        mintholderButton.onclick = async () => {
-            const initialBtnText = mintholderButton.textContent;
-            setButtonText(mintholderButton, "Loading...")
-            const quantity = getMintQuantity();
-
-            await mintHolder(quantity, getMintReferral()).then((r) => {
-                setButtonText(mintholderButton, "Mint more");
-                console.log(r);
-                showAlert(`Successfully minted ${quantity} NFTs`, "success")
-            }).catch((e) => {
-                console.log(e)
-                setButtonText(mintHolderButton, initialBtnText);
-                const { code, message } = parseTxError(e);
-                if (code !== 4001) {
-                    showAlert(`Minting error: ${message}. Please try again or contact us`, "error");
-                }
-            })
-        }
-    }
-}
-
-export const updateMintPresaleButton = () => {
-    const mintpresaleButton = document.querySelector('#presale-mint-button');
-    if (mintpresaleButton) {
-        mintpresaleButton.onclick = async () => {
-            const initialBtnText = mintpresaleButton.textContent;
-            setButtonText(mintpresaleButton, "Loading...")
-            const quantity = getMintQuantity();
-
-            await mintPresale(quantity, getMintReferral()).then((r) => {
-                setButtonText(mintpresaleButton, "Mint more");
-                console.log(r);
-                showAlert(`Successfully minted ${quantity} NFTs`, "success")
-            }).catch((e) => {
-                console.log(e)
-                setButtonText(mintPresaleButton, initialBtnText);
-                const { code, message } = parseTxError(e);
-                if (code !== 4001) {
-                    showAlert(`Minting error: ${message}. Please try again or contact us`, "error");
-                }
-            })
-        }
-    }
-}
-
 export const updateMintButton = () => {
     const mintButton = document.querySelector('#mint-button');
     if (mintButton) {
@@ -74,7 +26,6 @@ export const updateMintButton = () => {
     }
 }
 
-//Tiers - not sure what's up with these ones but will keep just in case
 export const updateMintByTierButtons = () => {
     const tierButtons = document.querySelectorAll('[tier]')
     if (!tierButtons.length)
@@ -99,7 +50,6 @@ export const updateMintByTierButtons = () => {
         }
     })
 }
-
 
 const getMintQuantity = () => {
     const quantity = document.querySelector('#quantity-select')?.value
