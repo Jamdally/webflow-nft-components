5c512963f9d43b02eec6594632fa67a36241519a
Jessica Dally <jessica.dally@me.com>
2021-12-21 13:20:10 -0700

    Update index.js

--- mint/index.js
+++ mint/index.js
@@ -1,4 +1,4 @@
-import {updateMintButton, updateMintByTierButtons} from "./ui.js";
+import {updateMintholderButton, updateMintpresaleButton, updateMintButton, updateMintByTierButtons} from "./ui.js";
 import { updateConnectButton, updateWalletStatus } from "../wallet.js";
 import { setContracts } from "../contract.js";
 import { renderAlertContainer } from "../ui/alerts.js";
