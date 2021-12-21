26d4a97a49aa26bd0f30e898446832ddb0bc0404
Jessica Dally <jessica.dally@me.com>
2021-12-21 16:06:11 -0700

    Update index.js

--- mint/index.js
+++ mint/index.js
@@ -1,4 +1,4 @@
-import {updateMintButton, updatePresaleButton, updateHolderButton, updateMintByTierButtons} from "./ui.js";
+import {updateMintButton, updateMintByTierButtons} from "./ui.js";
 import { updateConnectButton, updateWalletStatus } from "./wallet.js";
 import { setContracts } from "./contract.js";
 import { renderAlertContainer } from "./alerts.js";
@@ -8,8 +8,6 @@ export const init = async () => {
     await updateWalletStatus();
     await setContracts();
     updateConnectButton();
-    updateHolderButton();
-    updatePresaleButton();
     updateMintButton();	
     updateMintByTierButtons();
 }
